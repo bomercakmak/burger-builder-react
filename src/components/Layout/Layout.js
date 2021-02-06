da195419b6a9f68af5dea0c1712d3b0b2412a412
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-06 19:46:42 +0100

    I created the structure of the project. I write BurgerIngredient.js and CSS

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+import classes from './Layout.module.css'
+
+const Layout = (props) => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+  </Aux>
+)
+
+export default Layout;
