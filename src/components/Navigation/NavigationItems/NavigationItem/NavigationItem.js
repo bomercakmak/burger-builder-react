0ae590ba4340c22c81dbfbc6d6e3e5434baf2926
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-08 01:25:35 +0100

    I added NavigationItem and NavigationItems components.I write also css file for NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from './NavigationItem.module.css'
+const NavigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+        <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
+    </li>
+)
+
+export default NavigationItem
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const Toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+           <NavigationItems/>
         </nav>
     </header>
 )
