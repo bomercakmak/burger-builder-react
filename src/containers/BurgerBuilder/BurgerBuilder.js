0881f137af1bc710149e273677a729ab0a19ff63
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-10 15:24:49 +0100

    I I included axios in my project for make HTTP Request. I post some order data to Firebase.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
-
+import axios from '../../axios-orders'
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.7,
@@ -80,7 +80,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue !')
+        const order ={
+            ingredients:this.state.ingredients,
+            parice:this.state.totalPrice,
+            costumer:{
+                name:'Bahadir Omer',
+                address:{
+                    street: 'Teststreet 1',
+                    zipCode:'32442',
+                    country:'Germany'
+                },
+                email: 'bahadir0646@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+
+        }
+        axios.post('/orders.json',order)
+            .then(response =>{
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
     }
 
     render() {
