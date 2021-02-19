ee4422c3d65d0155a62b1186f870467de5aaa9b6
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-19 22:52:16 +0100

    I Fetch Orders and Output the Orders in orders page. I have a typo mistake in firebase price. I will be fix.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,30 @@ import classes from './Order.module.css'
 import React from 'react'
 const Order = (props) => {
 
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name:ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+
+    const ingredientOutput = ingredients.map(ig=>{
+        return <span 
+        style={{textTransform:'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding:'5px'}}
+        key={ig.name}>  {ig.name} ({ig.amount})</span>
+    })
+
 return(
+
     <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>EURO 5.45</strong></p>
+        <p>Ingredients: {ingredientOutput}</p>
+        <p>Price: <strong> {Number.parseFloat(props.price).toFixed(2)} €</strong></p>
     </div>
     )
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,41 @@
-import { Component} from 'react'
-import Order from '../../components/Order/Order'
+import axios from "../../axios-orders";
+import { Component } from "react";
+import Order from "../../components/Order/Order";
+
 class Orders extends Component {
- 
-    render() { 
-        return ( 
-            <div>
-            <Order/>
-            <Order/>
-            </div>
-         );
-    }
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios.get("/orders.json")
+      .then(res => {
+        const fetchedOrder = [];
+        for (let key in res.data) {
+          fetchedOrder.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrder });
+        console.log(this.state.orders);
+      })
+      .catch((err) => {
+          console.log("Fail");
+        this.setState({ loading: false });
+      });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.orders.map((order) => (
+          <Order key={order.id} 
+                  ingredients={order.ingredients}
+                  price={order.parice}/>
+        ))}
+      </div>
+    );
+  }
 }
- 
+
 export default Orders;
