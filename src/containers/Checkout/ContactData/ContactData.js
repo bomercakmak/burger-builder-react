793d1aec6f0c04e56ddd0f66368a153907ef2154
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-27 14:19:52 +0100

    I Adjusted to ContactData component to redux.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "../../../axios-orders";
+import {connect} from "react-redux"
 import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import Spinner from "../../../components/UI/Spinner/Spinner";
@@ -103,7 +104,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       parice: this.props.price,
       orderData: formData,
     };
@@ -199,4 +200,12 @@ class ContactData extends Component {
     );
   }
 }
-export default ContactData;
+
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.price
+  }
+}
+
+export default connect(mapStateToProps)(ContactData);
