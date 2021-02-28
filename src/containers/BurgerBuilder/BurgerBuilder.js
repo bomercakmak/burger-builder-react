269ff5d9adcbc5f999b5667b4968b419fc794042
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-02-28 14:05:59 +0100

    I Installed the Redux Devtools. I Prepared the Folder Structure and Created Action Creators.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,8 +8,7 @@ import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import Spinner from "../../components/UI/Spinner/Spinner";
-import * as actionTypes from "../../store/actions"
-
+import * as burgerBuilderActions from "../../store/actions/index";
 
 
 
@@ -121,8 +120,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT,ingredientName:ingName}),
-    onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT,ingredientName:ingName})
+    onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
-import reducer from "./store/reducer";
-
-const store = createStore(reducer);
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+ 
+const store = createStore(burgerBuilderReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
   <Provider store={store}>
