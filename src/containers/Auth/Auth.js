7395d8f7534df0a7c27b62a4a42af5dc5ded24f6
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-05 18:43:12 +0100

    I added the inputChangedHandler function  for an  Auth Form.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
-import classes from "./Auth.module.css"
+import classes from "./Auth.module.css";
 
 class Auth extends Component {
   state = {
@@ -37,6 +37,43 @@ class Auth extends Component {
     },
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (!rules) {
+      return true;
+    }
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.minLenght) {
+      isValid = value.length >= rules.minLenght && isValid;
+    }
+    if (rules.maxLenght) {
+      isValid = value.length <= rules.maxLenght && isValid;
+    }
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    this.setState({ controls: updatedControls });
+  };
 
   render() {
     const formElementsArray = [];
@@ -57,17 +94,14 @@ class Auth extends Component {
         invalid={!formElement.config.valid}
         touched={formElement.config.touched}
         changed={(event) => this.inputChangedHandler(event, formElement.id)}
-
       />
     ));
 
     return (
       <div className={classes.Auth}>
         <form>
-            {form}
-            <Button btnType="Success">SUBMIT</Button>
-
-
+          {form}
+          <Button btnType="Success">SUBMIT</Button>
         </form>
       </div>
     );
