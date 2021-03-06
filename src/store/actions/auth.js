aeef436a9c42c7818bf0c800affce1054a1fee55
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-06 16:17:55 +0100

    I Getting a Token from the Backend.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart())
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjZFnKVs1UnnRie3weB4XzSKfLeePrqfo',authData)
+        .then((response=>{
+            console.log(response);
+            dispatch(authSuccess(response.data))
+        }))
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail(err))
+        })
     }
 }
