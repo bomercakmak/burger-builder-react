018df73172427f2baf1d56492bdd97aa6073a258
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-11 13:57:54 +0100

    I added Guarding for not Authentication Routes.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter,Redirect } from "react-router-dom";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import Orders from './containers/Orders/Orders'
-import Auth from "./containers/Auth/Auth"
-import Logout from "./containers/Auth/Logout/Logout"
-import {connect} from "react-redux"
-import * as actions from "./store/actions/index"
+import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/"/>
+      </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+      );
+    }
     return (
       <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
   return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState())
-  }
-}
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
-export default withRouter (connect(null,mapDispatchToProps)(App));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,43 +1,43 @@
 import * as actionTypes from "./actionTypes";
 import axios from "../../axios-orders";
 
-export const addIngredient = (name) =>{
-    return{ 
-        type: actionTypes.ADD_INGREDIENT,
-        ingredientName:name
-    }
-}
-export const removeIngredient = (name) =>{
-    return{ 
-        type: actionTypes.REMOVE_INGREDIENT,
-        ingredientName:name
-    }
-}
+export const addIngredient = (name) => {
+  return {
+    type: actionTypes.ADD_INGREDIENT,
+    ingredientName: name,
+  };
+};
+export const removeIngredient = (name) => {
+  return {
+    type: actionTypes.REMOVE_INGREDIENT,
+    ingredientName: name,
+  };
+};
 
-export const setIngredients = (ingredients) =>{
-    return{
-        type: actionTypes.SET_INGREDIENTS,
-        ingredients:ingredients
-    }
-}
+export const setIngredients = (ingredients) => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    ingredients: ingredients,
+  };
+};
 
 export const fetchedIngredientsFailed = () => {
-    return{
-        type: actionTypes.FETCH_INGREDIENT_FAILED
-    }
-}
+  return {
+    type: actionTypes.FETCH_INGREDIENT_FAILED,
+  };
+};
 
 export const initIngredients = () => {
-    return dispatch => {
-        axios
-        .get(
-          "https://burger-builder-react-28c1e-default-rtdb.firebaseio.com/ingredients.json"
-        )
-        .then((response) => {
-           dispatch(setIngredients(response.data))
-        })
-        .catch((error) => {
-            dispatch(fetchedIngredientsFailed())
-        });
-    }
-}
+  return (dispatch) => {
+    axios
+      .get(
+        "https://burger-builder-react-28c1e-default-rtdb.firebaseio.com/ingredients.json"
+      )
+      .then((response) => {
+        dispatch(setIngredients(response.data));
+      })
+      .catch((error) => {
+        dispatch(fetchedIngredientsFailed());
+      });
+  };
+};
