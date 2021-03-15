2ca04428b142ae1f08a2a8ff808d653f569265c8
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-15 14:46:02 +0100

    I tested Containers and Auth Reducer.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as actions from "../../store/actions/index";
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false
   };
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,38 @@
+import reducer from "./auth";
+import * as ActionTypes from "../actions/actionTypes";
+
+describe("auth reducer", () => {
+  it("should return the initialState", () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: "/",
+    });
+  });
+  it("should store the token upon login", () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: "/",
+        },
+        {
+          type: ActionTypes.AUTH_SUCCESS,
+          idToken: "some-token",
+          userId: "some-user-id",
+        }
+      )
+    ).toEqual({
+      token: "some-token",
+      userId: "some-user-id",
+      error: null,
+      loading: false,
+      authRedirectPath: "/",
+    });
+  });
+});
